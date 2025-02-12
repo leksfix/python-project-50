cf281638b8bdfe8a29184e161f9189d9151c0b3d
alex <leksfix@gmail.com>
2025-02-12 16:13:05 +0100

    Fixed linter messages

--- gendiff/compare.py
+++ gendiff/compare.py
@@ -8,9 +8,9 @@ def get_diff(dict1, dict2):
     """Assume that input parameters are dicts"""
     res = []
     for k in dict2.keys() | dict1.keys():
-        if not k in dict1.keys():
+        if k not in dict1.keys():
             res.append({'type': DIFF_TYPE_ADD, 'name': k, 'value': dict2[k]})
-        elif not k in dict2.keys():
+        elif k not in dict2.keys():
             res.append({'type': DIFF_TYPE_DEL, 'name': k, 'value': dict1[k]})
         elif dict1[k] == dict2[k]:
             res.append({'type': DIFF_TYPE_UNC, 'name': k, 'value': dict1[k]})
